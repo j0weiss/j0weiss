1d3f18ec77271c12df8202aa158d0bc9cd218ccd
j0weiss <j0weiss@icloud.com>
2019-03-19 08:14:31 +0100

    Open links in new tab

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -28,7 +28,7 @@ const URL = styled.p`
 class LinkCard extends Component {
   render() {
     return (
-      <Card href={this.props.url}>
+      <Card href={this.props.url} target="_blank" rel="noopener noreferrer">
         <Label>{this.props.label}</Label>
         <URL>{this.props.url}</URL>
       </Card>
